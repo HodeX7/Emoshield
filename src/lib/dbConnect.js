a7e90b27ef56e706bf74b38d1fab9270acd6ced1
Akshar Mehta <63180150+HodeX7@users.noreply.github.com>
2024-11-08 23:31:14 -0800

    Added DB connect File

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,30 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGODB_URI = "mongodb+srv://akshar:loAcmgqdylo3lRVu@emoshield.ssm7l.mongodb.net/?retryWrites=true&w=majority&appName=Emoshield";
-console.log(process.env.MONGODB_URI)
+const MONGODB_URI =
+  "mongodb+srv://akshar:loAcmgqdylo3lRVu@emoshield.ssm7l.mongodb.net/?retryWrites=true&w=majority&appName=Emoshield";
+console.log(process.env.MONGODB_URI);
 
 if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable');
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-            return mongoose;
-        });
-    }
-    cached.conn = await cached.promise;
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 module.exports = dbConnect;
